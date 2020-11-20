291ba0b943e68fb44ac05c67b96f84fe5029408f
Dixon Liang <liang.dixon@gmail.com>
2020-11-19 23:12:11 -0500

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -2,10 +2,13 @@ import tweepy
 from textblob import TextBlob
 import numpy as np
 
+consumer_key =
+consumer_secret =
+access_token =
+access_token_secret =
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-
 
 api = tweepy.API(auth,wait_on_rate_limit=True)
 
